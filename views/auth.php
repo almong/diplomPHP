cb29a89a549c8da6880bee3501949500e0a26b3c
almong <admin@ag-net.ru>
2018-10-12 15:36:27 +0300

    update faq.sql

--- views/auth.php
+++ views/auth.php
@@ -5,7 +5,7 @@ include 'header.php';
 
 <section>
     <h1 class="h1 text-center">Авторизация</h1>
-    <form method="post" action="/auth" class="col-3 mx-auto">
+    <form method="post" action="/admin" class="col-3 mx-auto">
         <div class="form-group">
             <label for="exampleInputLogin1">login</label>
             <input name="login" type="text" class="form-control" id="exampleInputEmail1"">
